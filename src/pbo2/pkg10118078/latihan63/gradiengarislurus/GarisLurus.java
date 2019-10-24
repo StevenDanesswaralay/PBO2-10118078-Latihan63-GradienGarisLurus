c48e20371e10a2d686374cef6af4c5b473507dca
Steven Danesswaralay <55594713+StevenDanesswaralay@users.noreply.github.com>
2019-10-24 09:05:31 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118078/latihan63/gradiengarislurus/GarisLurus.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118078.latihan63.gradiengarislurus;
+
+/**
+ *
+ * @author 
+ * Nama     : Steven Danesswaralay
+ * Kelas    : PBO2
+ * Nim      : 10118078
+ * Deskripsi Tugas : Gradien Garis Lurus
+ */
+public interface GarisLurus {
+    public int hitungGradien();
+}
